0fe29a99c6044439911e36d0cbab15a815a385e3
Ruins He <lyhypacm@gmail.com>
2020-02-25 21:39:38 +0800

    Features((db)): Add team database creating script and model.
    Add the team as non-snapshot team, and we will designed using snapshot for contest history (i.e.
    scoreboard).
    
    re #1
    
    Signed-off-by: Ruins He <lyhypacm@gmail.com>

---
+++ app/model/team/team.go
@@ -0,0 +1,3 @@
+package team
+
+// Fill with you ideas below.
--- scripts/acm-training.sql
+++ scripts/acm-training.sql
@@ -36,3 +36,11 @@ CREATE TABLE IF NOT EXISTS `contest` (
     `start_time` BIGINT NOT NULL COMMENT 'the start time of the contest, as unix timestamp',
     `duration` BIGINT NOT NULL DEFAULT 1500 COMMENT 'the duration of the contest, unit in seconds'
   ) ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `team` (
+    `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(32) NOT NULL,
+    `captain` BIGINT NOT NULL COMMENT 'the id of the captain user',
+    -- Since we only three members in the team, keep the table simple as an internal usage system.
+    `member1` BIGINT DEFAULT NULL COMMENT 'the id of the first member',
+    `member2` BIGINT DEFAULT NULL COMMENT 'the id of the second member'
+  ) ENGINE = InnoDB;
